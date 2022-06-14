b03c1be231042347b33958ffe91d58d2f8e7a923
Juraj Oberta <jurajo80@gmail.com>
2022-06-14 18:59:07 +0200

    Implementing real-time sentence vectorization

--- DatasetEncoding.py
+++ DatasetEncoding.py
@@ -1,4 +1,15 @@
 from Encoding.Encoders import EncoderClassifier
+from Vectorization.Vectorizer import Vectorizer
 
-encoder = EncoderClassifier()
-encoder.encode_feature_set('./data/otazky.csv', './encoded/otazky_encoded')
+#encoder = EncoderClassifier()
+#encoder.encode_feature_set('./data/otazky.csv', './encoded/otazky_encoded')
+
+vectorizer = Vectorizer('./encoded/otazky_encoded_dictionary.dat')
+while True:
+    input_sentence = input()
+    if (input_sentence == 'e'):
+        break
+
+    vector = vectorizer.vectorize_sentence(input_sentence)
+    print(vector)
+    print(vectorizer.resentence(vector))
